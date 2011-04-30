b21305cbd4274a3ee2b94dca2612c8cb61fbc250
Bane <bane@linux.cn>
2011-04-30 23:14:31 +0800

    给bigdigits.py增加更多注释说明，方便理解

--- Programming_in_Python3/Chapter1/bigdigits.py
+++ Programming_in_Python3/Chapter1/bigdigits.py
@@ -26,15 +26,16 @@ Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
 Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
 try:
+#获取命令选中的第2个参数
     digits = sys.argv[1]
     row = 0
-    while row < 7:
+    while row < 7:	#因为我们使用7行的星号来表示一个数字
         line = ""
         column = 0
-        while column < len(digits):
-            number = int(digits[column])
-            digit = Digits[number]
-            line += digit[row] + "  "
+        while column < len(digits):	#len(digits)获取是的字符串长度
+            number = int(digits[column])	#半第2个参数中的字符转换成数字
+            digit = Digits[number]	#取得大数字对象
+            line += digit[row] + "  "	#打印大数字的第row行
             column += 1
         print(line)
         row += 1
